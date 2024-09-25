0b976f01454515e9995077bb7e80df1c26070a17
Luiz Felipe <luizfelipesmaciel@gmail.com>
2024-09-25 18:14:00 -0300

    Create python script to send a valid payload in loop

---
+++ functions/send_to_topic.py
@@ -0,0 +1,30 @@
+from itertools import cycle
+import json
+import os
+from time import sleep
+
+import boto3
+
+
+def lambda_handler(payload, context):
+
+    topic_name = os.getenv('TOPIC_NAME')
+    print(payload)
+
+    res = boto3.client('iot-data').publish(
+        topic=topic_name,
+        payload=payload
+    )
+    return res
+
+
+if __name__ == '__main__':
+
+    colors = ['blue', 'red', 'yellow', 'green']
+
+    for color in cycle(colors):
+        lambda_handler(json.dumps({'led': color, 'state': 1}), None)
+        print(f'Published: {color} - 1')
+        sleep(.5)
+        lambda_handler(json.dumps({'led': color, 'state': 0}), None)
+        print(f'Published: {color} - 0')
