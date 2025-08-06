5aa502d3fa32ee48cd6e8bfdd64be191e394e932
Luiz Felipe <luizfelipesmaciel@gmail.com>
2025-08-06 16:09:50 -0300

    match last_hash

--- functions/send_to_topic.py
+++ functions/send_to_topic.py
@@ -1,10 +1,51 @@
+import http.client
 from itertools import cycle
 import hashlib
 import json
 import os
 from time import sleep
+from urllib.parse import urlparse
 
 import boto3
+
+
+def split_url(url):
+    parsed = urlparse(url)
+    host = parsed.netloc
+    path = parsed.path or "/"
+
+    return host, path
+
+
+def get_last_hash(callback_url, retries=3, delay=1):
+    attempt = 0
+
+    host, path = split_url(callback_url)
+
+    while attempt < retries:
+        try:
+            print(f"Tentativa {attempt + 1} de {retries}...")
+            conn = http.client.HTTPSConnection(host, timeout=2)
+            conn.request("GET", path)
+            response = conn.getresponse()
+            data = response.read().decode("utf-8")
+            conn.close()
+
+            if 200 <= response.status < 300:
+                last_hash = json.loads(data)['hash']
+                return last_hash
+
+            raise Exception(f"Erro HTTP {response.status}")
+
+        except Exception as e:
+            print(f"Erro: {e}")
+            attempt += 1
+            if attempt < retries:
+                print(f"Aguardando {delay} segundos para nova tentativa...")
+                time.sleep(delay)
+            else:
+                print("Máximo de tentativas atingido. Falha na requisição.")
+                return None
 
 
 def generate_hash(payload):
@@ -13,7 +54,6 @@ def generate_hash(payload):
     hash_md5 = hashlib.md5(json_bytes).hexdigest()
 
     return hash_md5
-
 
 def lambda_handler(payload, context):
 
@@ -33,20 +73,27 @@ def lambda_handler(payload, context):
 
     # make HTTP GET /lastHash to see if matches with mash_md5
 
-    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
-        return {
-            "statusCode": 200,
-            "contentType": "application/json",
-            "body": json.dumps({
-                "message": f'Success changing {payload["led"]} LED state to {payload["state"]}'
-            })
-        }
+    assert res['ResponseMetadata']['HTTPStatusCode'] == 200
+
+    count = 1
+    max_tries = 3
+    while count <= max_tries:
+        last_hash = get_last_hash(payload['callbackUrl'])
+        if last_hash == hash_md5:
+            return {
+                "statusCode": 200,
+                "contentType": "application/json",
+                "body": json.dumps({
+                    "message": f'Request Id {last_hash} succeeded'
+                })
+            }
+        sleep(.5)
 
     return {
         "statusCode": 500,
         "contentType": "application/json",
         "body": json.dumps({
-            "message": 'Unable to change LED state'
+            "message": 'Request Id {last_hash} was unable to complete'
         })
     }
 
